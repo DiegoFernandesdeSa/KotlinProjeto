2cbb11876a340bed442a10e6ed201a6aec7205ff
DiegoFernandesdeSa <diegofernandesdesa@gmail.com>
2021-03-18 21:44:06 -0300

    create person class

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,14 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+    var name: String = "Diego"
+    var CPF: String = "383.876.908.-21"
+
+}
+
+fun main() {
+    val Diego = Pessoa()
+
+    println(Diego.name)
+    println(Diego.CPF)
+}
