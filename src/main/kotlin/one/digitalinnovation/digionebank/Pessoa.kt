1a40b9cd790566fd6a88adad7ce02e14149609df
DiegoFernandesdeSa <diegofernandesdesa@gmail.com>
2021-03-18 22:25:32 -0300

    update to talk about class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,17 +2,19 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var name: String = "Diego"
-
     var CPF: String = "383.876.908.-21"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$name + $CPF"
 
 }
 
 fun main() {
     val Diego = Pessoa()
 
-    println(Diego.name)
-    println(Diego.CPF)
+    println(Diego.pessoaInfo())
 
 
 }
