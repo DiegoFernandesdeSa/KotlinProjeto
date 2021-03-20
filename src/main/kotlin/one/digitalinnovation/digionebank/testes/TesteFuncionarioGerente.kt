c6eb87261a104e0f095bef74261f042deb53f63e
DiegoFernandesdeSa <diegofernandesdesa@gmail.com>
2021-03-20 18:34:13 -0300

    Finalização do módulo com criação de Polimorfismo.

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -3,8 +3,12 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
-    val Maria = Gerente("Maria do Carmo", "123.456.786-99", 5000.0)
+    val Maria = Gerente("Maria do Carmo", "123.456.786-99", 5000.0, "senha123")
 
     ImprimiRelatorioFuncionarios.imprime(Maria)
 
+    TesteAutenticacao().Autentica(Maria)
+
 }
+
+
