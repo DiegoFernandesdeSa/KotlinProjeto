3541bf99fe088ff72891d9c4d990879563acf007
DiegoFernandesdeSa <diegofernandesdesa@gmail.com>
2021-03-19 23:51:17 -0300

    Created Enum Client Type and test to it

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,20 +1,7 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var name: String = "Diego"
-    var CPF: String = "222.222.222-21"
-    private set
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
 
-    constructor()
-
-    fun pessoaInfo() = "$name + $CPF"
-
-}
-
-fun main() {
-    val Diego = Pessoa()
-
-    println(Diego.pessoaInfo())
-
-
-}
+)
